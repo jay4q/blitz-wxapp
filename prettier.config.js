95a7357bfb276fb18adf4614bd6b89eab657de39
jay4q <jay4qqqq@gmail.com>
2023-04-21 15:41:52 +0800

    refactor: 🎸 优化 prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,23 +1,3 @@
-// https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31#issuecomment-1024722576
-const tailwind = require('prettier-plugin-tailwindcss')
-const organizeImports = require('@trivago/prettier-plugin-sort-imports')
-
-const combinedFormatter = {
-  ...tailwind,
-  parsers: {
-    ...tailwind.parsers,
-    ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
-      acc[key] = {
-        ...tailwind.parsers[key],
-        preprocess(code, options) {
-          return organizeImports.parsers[key].preprocess(code, options)
-        },
-      }
-      return acc
-    }, {}),
-  },
-}
-
 module.exports = {
   semi: false,
   singleQuote: true,
@@ -25,7 +5,6 @@ module.exports = {
   jsxBracketSameLine: false,
   arrowParens: 'always',
   printWidth: 128,
-  plugins: [combinedFormatter],
-
-  importOrder: ['^@(.*)/(.*)$', '^[^(@|./)]*$', '^[./]'],
+  plugins: ['prettier-plugin-jsdoc', '@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  pluginSearchDirs: false,
 }
